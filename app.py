93db467b85f28643f867c6f954c41f128bdadeae
changhee <change@hphk.kr>
2018-11-07 05:04:18 +0000

    index

--- app.py
+++ app.py
@@ -18,19 +18,30 @@ def index():
     todos = Todo.query.order_by(Todo.deadline.asc()).all()
     return render_template('index.html',todos=todos)
     
-@app.route('/posts/new')
-def new():
+# @app.route('/posts/new')
+# def new():
+#     return render_template('new.html')
+    
+# @app.route('/posts/create', methods=['POST'])
+# def create():
+#     # 사용자가 입력한 데이터 가져오기
+#     todo = request.form['todo']
+#     deadline = request.form.get('deadline')
+#     # 가져온 데이터로 Todo 만들기
+#     todo = Todo(todo,deadline)
+#     # ToDo DB에 저장하기
+#     db.session.add(todo)
+#     db.session.commit()
+#     # 어느 페이지로 이동할지 정하기
+#     return redirect('/')
+
+@app.route('/todos/create', methods=['POST','GET'])
+def todo():
+    if request.method == "POST":
+        todo = Todo(request.form['todo'],request.form['deadline'])
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+        
     return render_template('new.html')
     
-@app.route('/posts/create', methods=['POST'])
-def create():
-    # 사용자가 입력한 데이터 가져오기
-    todo = request.form['todo']
-    deadline = request.form.get('deadline')
-    # 가져온 데이터로 Todo 만들기
-    todo = Todo(todo,deadline)
-    # ToDo DB에 저장하기
-    db.session.add(todo)
-    db.session.commit()
-    # 어느 페이지로 이동할지 정하기
-    return redirect('/')
