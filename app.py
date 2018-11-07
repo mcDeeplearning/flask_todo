7365915e5dd4b6af3160a7118773c3094d722365
changhee <change@hphk.kr>
2018-11-07 04:13:56 +0000

    CR구현

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -15,15 +15,22 @@ migrate = Migrate(app,db)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    todos = Todo.query.order_by(Todo.deadline.asc()).all()
+    return render_template('index.html',todos=todos)
     
 @app.route('/posts/new')
 def new():
     return render_template('new.html')
     
-@app.route('/posts/create')
+@app.route('/posts/create', methods=['POST'])
 def create():
     # 사용자가 입력한 데이터 가져오기
+    todo = request.form['todo']
+    deadline = request.form.get('deadline')
     # 가져온 데이터로 Todo 만들기
+    todo = Todo(todo,deadline)
     # ToDo DB에 저장하기
+    db.session.add(todo)
+    db.session.commit()
     # 어느 페이지로 이동할지 정하기
+    return redirect('/')
